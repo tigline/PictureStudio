c0954d5d54a2753330a9e1ca209bdc9f83590dc6
Aaron Hou <Aaron.Hou@harman.com>
2018-03-20 16:41:44 +0800

    delete pods

--- Pods/Target Support Files/Pods-PictureStudio/Pods-PictureStudio-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PictureStudio : NSObject
-@end
-@implementation PodsDummy_Pods_PictureStudio
-@end
--- Pods/Target Support Files/Pods-PictureStudioTests/Pods-PictureStudioTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PictureStudioTests : NSObject
-@end
-@implementation PodsDummy_Pods_PictureStudioTests
-@end
