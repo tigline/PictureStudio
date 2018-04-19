b566ebbce545b017f7ff46919893a7ce58e34e98
Aaron Hou <Aaron.Hou@harman.com>
2018-04-19 12:01:22 +0800

    modify

--- PictureStudio/model/CombinePicture.mm
+++ PictureStudio/model/CombinePicture.mm
@@ -95,7 +95,7 @@ Point2f getTransformPoint(const Point2f originalPoint,const Mat &transformMaxtri
         //获取图像1到图像2的投影映射矩阵，尺寸为3*3
         Mat homo=findHomography(imagePoints1,imagePoints2,CV_RANSAC);
 
-        Mat adjustMat=(Mat_<double>(3,3)<<1.0,0,0,0,1.0,image01.rows,0,0,1.0);
+        Mat adjustMat=(Mat_<double>(3,3)<<1.0,0,0,  0,1.0,image01.rows,  0,0,1.0);
 
         Mat adjustHomo=adjustMat*homo;
 
@@ -103,45 +103,63 @@ Point2f getTransformPoint(const Point2f originalPoint,const Mat &transformMaxtri
         //获取最强配对点在原始图像和矩阵变换后图像上的对应位置，用于图像拼接点的定位
         Point2f originalLinkPoint,targetLinkPoint,basedImagePoint;
         
+    
+        
         originalLinkPoint=keyPoint1[matchePoints[0].queryIdx].pt;
-        NSLog(@"originalLinkPoint.y = %f",originalLinkPoint.y);
+        
+        NSLog(@"originalLinkPointx.y = %f / %f",originalLinkPoint.x, originalLinkPoint.y);
         
         targetLinkPoint=getTransformPoint(originalLinkPoint,adjustHomo);
-        NSLog(@"targetLinkPoint.y = %f",targetLinkPoint.y);
+        NSLog(@"targetLinkPointx.y = %f / %f",targetLinkPoint.x, targetLinkPoint.y);
         
         basedImagePoint=keyPoint2[matchePoints[0].trainIdx].pt;
-        NSLog(@"basedImagePoint.y = %f",basedImagePoint.y);
+        NSLog(@"basedImagePointx.y = %f / %f",basedImagePoint.x, basedImagePoint.y);
+        int index = 0;
+        while (basedImagePoint.x != originalLinkPoint.x) {
+            if (index < 10) {
+                index ++;
+                originalLinkPoint=keyPoint1[matchePoints[index].queryIdx].pt;
+            }
+            else
+            {
+                originalLinkPoint=keyPoint1[matchePoints[0].queryIdx].pt;
+                break;
+            }
+        }
+        
         
         //Mat imageTransform;
         //图像配准
-        //warpPerspective(image01,imageTransform1,adjustMat*homo,cv::Size(image02.cols,image01.rows+image02.rows));
+        warpPerspective(image01,imageTransform1,adjustMat*homo,cv::Size(image02.cols,image01.rows+image02.rows));
         if(basedImagePoint.y > originalLinkPoint.y) {
             resultMat = comMatC(image01, image002, resultMat);
             continue;
         }
-        
-//        Mat image1Overlap, image2Overlap; //图1和图2的重叠部分
-//        image1Overlap = image01(cv::Rect(cv::Point(0, originalLinkPoint.y - basedImagePoint.y), cv::Point(image01.cols, originalLinkPoint.y)));
-//        UIImage *imageTransform04 = [self imageWithCVMat:image1Overlap];
-//        image2Overlap = image02(cv::Rect(0, 0, image1Overlap.cols, image1Overlap.rows));
-//
-//        Mat image1ROICopy = image1Overlap.clone();  //复制一份图1的重叠部分
-//        UIImage *imageTransform05 = [self imageWithCVMat:image2Overlap];
-//        for (int j = 0; j<image1Overlap.cols; j++)
-//        {
-//            for (int i = 0; i<image1Overlap.rows; i++)
-//            {
-//                double height;
-//                height = (double)j / image1Overlap.rows;  //随距离改变而改变的叠加系数
-//                image1Overlap.at<Vec3b>(i, j)[0] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[0] + height*image2Overlap.at<Vec3b>(i, j)[0];
-//                image1Overlap.at<Vec3b>(i, j)[1] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[1] + height*image2Overlap.at<Vec3b>(i, j)[1];
-//                image1Overlap.at<Vec3b>(i, j)[2] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[2] + height*image2Overlap.at<Vec3b>(i, j)[2];
-//            }
-//        }
+        UIImage *imageTransform06 = [self imageWithCVMat:imageTransform1];
+        Mat image1Overlap, image2Overlap; //图1和图2的重叠部分
+       // image1Overlap = image01(cv::Rect(cv::Point(0, originalLinkPoint.y - basedImagePoint.y), cv::Point(image01.cols, originalLinkPoint.y)));
+        image1Overlap = imageTransform1(cv::Rect(cv::Point(0, targetLinkPoint.y - basedImagePoint.y), cv::Point(image02.cols, targetLinkPoint.y)));
+        UIImage *imageTransform04 = [self imageWithCVMat:image1Overlap];
+        image2Overlap = image02(cv::Rect(0, 0, image1Overlap.cols, image1Overlap.rows));
+
+        Mat image1ROICopy = image1Overlap.clone();  //复制一份图1的重叠部分
+        UIImage *imageTransform05 = [self imageWithCVMat:image2Overlap];
+        for (int j = 0; j<image1Overlap.cols; j++)
+        {
+            for (int i = 0; i<image1Overlap.rows; i++)
+            {
+                double height;
+                height = (double)j / image1Overlap.rows;  //随距离改变而改变的叠加系数
+                image1Overlap.at<Vec3b>(i, j)[0] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[0] + height*image2Overlap.at<Vec3b>(i, j)[0];
+                image1Overlap.at<Vec3b>(i, j)[1] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[1] + height*image2Overlap.at<Vec3b>(i, j)[1];
+                image1Overlap.at<Vec3b>(i, j)[2] = (1 - height)*image1ROICopy.at<Vec3b>(i, j)[2] + height*image2Overlap.at<Vec3b>(i, j)[2];
+            }
+        }
         //在最强匹配点的位置处衔接，最强匹配点上侧是图1，下侧是图2，这样直接替换图像衔接不好，光线有突变
         //不重合的部分
         //UIImage *imageTransform03 = [self imageWithCVMat:image1Overlap];
-        Mat cutImage01 = image01(cv::Rect(cv::Point(0,0), cv::Point(image01.cols, image01.rows - basedImagePoint.y - 79)));
+        //Mat cutImage01 = image01(cv::Rect(cv::Point(0,0), cv::Point(image01.cols, image01.rows - basedImagePoint.y - 79)));
+        Mat cutImage01 = image01(cv::Rect(cv::Point(0,0), cv::Point(image01.cols, originalLinkPoint.y)));
         UIImage *cutImage1 = [self imageWithCVMat:cutImage01];
         Mat ROIMat=image02(cv::Rect(cv::Point(0,basedImagePoint.y),cv::Point(image02.cols,image02.rows)));
         UIImage *imageTransform01 = [self imageWithCVMat:ROIMat];
