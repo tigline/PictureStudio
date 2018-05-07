67d1e54ce24f0495210a99e6d2a35caf0dc9fca8
tigline <mickey@mickeydeMacBook-Pro.local>
2018-05-08 00:38:39 +0800

    集成友盟

--- PictureStudio/AppDelegate.m
+++ PictureStudio/AppDelegate.m
@@ -8,15 +8,20 @@
 
 #import "AppDelegate.h"
 #import "ViewController.h"
-#import "WXApi.h"
+//#import "WXApi.h"
+
+
+
 //微信开发者ID
 #define URL_APPID @"wx3b864b92dca2bf8a"
+#define UMeng_AppKey @"5af056188f4a9d1da00000f7"
+
 void uncaughtExceptionHandler(NSException *exception) {
     NSSLog(@"CRASH: %@", exception);
     NSSLog(@"Stack Trace: %@", [exception callStackSymbols]);
     // Internal error reporting
 }
-@interface AppDelegate ()<WXApiDelegate>
+@interface AppDelegate ()
 
 @end
 
@@ -38,13 +43,50 @@ void uncaughtExceptionHandler(NSException *exception) {
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
     
     //向微信注册应用。
-    [WXApi registerApp:URL_APPID ];
+    //[WXApi registerApp:URL_APPID ];
+    [self configUSharePlatforms];
+    [self confitUShareSettings];
     return YES;
+}
+
+- (void)confitUShareSettings
+{
+    [UMConfigure initWithAppkey:UMeng_AppKey channel:@"App Store"];
+    [UMConfigure setLogEnabled:YES];
+    /*
+     * 打开图片水印
+     */
+    //[UMSocialGlobal shareInstance].isUsingWaterMark = YES;
+    /*
+     * 关闭强制验证https，可允许http图片分享，但需要在info.plist设置安全域名
+     <key>NSAppTransportSecurity</key>
+     <dict>
+     <key>NSAllowsArbitraryLoads</key>
+     <true/>
+     </dict>
+     */
+    //[UMSocialGlobal shareInstance].isUsingHttpsWhenShareContent = NO;
+}
+- (void)configUSharePlatforms
+{
+    /* 设置微信的appKey和appSecret */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_WechatSession appKey:@"wx3b864b92dca2bf8a" appSecret:@"3baf1193c85774b3fd9d18447d76cab0" redirectURL:@"http://mobile.umeng.com/social"];
+    /*
+     * 移除相应平台的分享，如微信收藏
+     */
+    //[[UMSocialManager defaultManager] removePlatformProviderWithPlatformTypes:@[@(UMSocialPlatformType_WechatFavorite)]];
+    /* 设置分享到QQ互联的appID
+     * U-Share SDK为了兼容大部分平台命名，统一用appKey和appSecret进行参数设置，而QQ平台仅需将appID作为U-Share的appKey参数传进即可。
+     */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_QQ appKey:@"1105821097"/*设置QQ平台的appID*/  appSecret:nil redirectURL:@"http://mobile.umeng.com/social"];
+    /* 设置新浪的appKey和appSecret */
+    [[UMSocialManager defaultManager] setPlaform:UMSocialPlatformType_Sina appKey:@"3921700954"  appSecret:@"04b48b094faeb16683c32669824ebdad" redirectURL:@"https://sns.whalecloud.com/sina2/callback"];
+
 }
 
 
 -(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
-    
+
     /*! @brief 处理微信通过URL启动App时传递的数据
      *
      * 需要在 application:openURL:sourceApplication:annotation:或者application:handleOpenURL中调用。
@@ -52,16 +94,31 @@ void uncaughtExceptionHandler(NSException *exception) {
      * @param delegate  WXApiDelegate对象，用来接收微信触发的消息。
      * @return 成功返回YES，失败返回NO。
      */
-    return [WXApi handleOpenURL:url delegate:self];
+    //return [WXApi handleOpenURL:url delegate:self];
+    BOOL result = [[UMSocialManager defaultManager]  handleOpenURL:url options:options];
+    if (!result) {
+        // 其他如支付等SDK的回调
+    }
+    return result;
 }
-
-
+//
+//
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url{
-    return [WXApi handleOpenURL:url delegate:self];
+    //return [WXApi handleOpenURL:url delegate:self];
+    BOOL result = [[UMSocialManager defaultManager] handleOpenURL:url];
+    if (!result) {
+        // 其他如支付等SDK的回调
+    }
+    return result;
 }
-
+//
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(nullable NSString *)sourceApplication annotation:(id)annotation{
-    return [WXApi handleOpenURL:url delegate:self];
+    //return [WXApi handleOpenURL:url delegate:self];
+    BOOL result = [[UMSocialManager defaultManager] handleOpenURL:url sourceApplication:sourceApplication annotation:annotation];
+    if (!result) {
+        // 其他如支付等SDK的回调
+    }
+    return result;
 }
 
 /*! 微信回调，不管是登录还是分享成功与否，都是走这个方法 @brief 发送一个sendReq后，收到微信的回应
@@ -70,31 +127,31 @@ void uncaughtExceptionHandler(NSException *exception) {
  * 可能收到的处理结果有SendMessageToWXResp、SendAuthResp等。
  * @param resp 具体的回应内容，是自动释放的
  */
--(void) onResp:(BaseResp*)resp{
-    NSLog(@"resp %d",resp.errCode);
-    
-    /*
-     enum  WXErrCode {
-     WXSuccess           = 0,    成功
-     WXErrCodeCommon     = -1,  普通错误类型
-     WXErrCodeUserCancel = -2,    用户点击取消并返回
-     WXErrCodeSentFail   = -3,   发送失败
-     WXErrCodeAuthDeny   = -4,    授权失败
-     WXErrCodeUnsupport  = -5,   微信不支持
-     };
-     */
-    
-    if ([resp isKindOfClass:[SendMessageToWXResp class]]) { //微信分享 微信回应给第三方应用程序的类
-        SendMessageToWXResp *response = (SendMessageToWXResp *)resp;
-        NSLog(@"error code %d  error msg %@  lang %@   country %@",response.errCode,response.errStr,response.lang,response.country);
-        //这里处理回调的方法 。 通过代理吧对应的登录消息传送过去。
-        if (_wxDelegate) {
-            if([_wxDelegate respondsToSelector:@selector(shareReturnByCode:)]){
-                [_wxDelegate shareReturnByCode:response.errCode];
-            }
-        }
-    }
-}
+//-(void) onResp:(BaseResp*)resp{
+//    NSLog(@"resp %d",resp.errCode);
+//
+//    /*
+//     enum  WXErrCode {
+//     WXSuccess           = 0,    成功
+//     WXErrCodeCommon     = -1,  普通错误类型
+//     WXErrCodeUserCancel = -2,    用户点击取消并返回
+//     WXErrCodeSentFail   = -3,   发送失败
+//     WXErrCodeAuthDeny   = -4,    授权失败
+//     WXErrCodeUnsupport  = -5,   微信不支持
+//     };
+//     */
+//
+//    if ([resp isKindOfClass:[SendMessageToWXResp class]]) { //微信分享 微信回应给第三方应用程序的类
+//        SendMessageToWXResp *response = (SendMessageToWXResp *)resp;
+//        NSLog(@"error code %d  error msg %@  lang %@   country %@",response.errCode,response.errStr,response.lang,response.country);
+//        //这里处理回调的方法 。 通过代理吧对应的登录消息传送过去。
+//        if (_wxDelegate) {
+//            if([_wxDelegate respondsToSelector:@selector(shareReturnByCode:)]){
+//                [_wxDelegate shareReturnByCode:response.errCode];
+//            }
+//        }
+//    }
+//}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ PictureStudio/view/ShareBoardView.h
@@ -0,0 +1,16 @@
+//
+//  ShareBoardView.h
+//  PictureStudio
+//
+//  Created by mickey on 2018/5/7.
+//  Copyright © 2018年 Aaron Hou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareBoardView : UIView
+
+- (void)showShareBoard:(UIView *)rootView;
+- (void)hideShareBoard:(UIView *)rootView;
+
+@end
